d3e32124be2a617d860e578a2d0957932e7199b7
Jeong Suhyeon <jsh00325@naver.com>
2024-11-23 01:55:04 +0900

    Design: 모델 선택 페이지 UI 구현

--- app/src/main/java/com/classification/handwriting/presentation/select_model/SelectModelActivity.kt
+++ app/src/main/java/com/classification/handwriting/presentation/select_model/SelectModelActivity.kt
@@ -2,12 +2,15 @@ package com.classification.handwriting.presentation.select_model
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.classification.handwriting.R
+import com.classification.handwriting.databinding.ActivitySelectModelBinding
 
 class SelectModelActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySelectModelBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_model)
+        binding = ActivitySelectModelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
