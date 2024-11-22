1190b197ba4f3e5c4590cd9b83bd886afe5660b3
Jeong Suhyeon <jsh00325@naver.com>
2024-11-23 01:55:38 +0900

    Feat: 항상 라이트모드로 설정하는 기눙 추가

--- app/src/main/java/com/classification/handwriting/HandwritingClassificationApplication.kt
+++ app/src/main/java/com/classification/handwriting/HandwritingClassificationApplication.kt
@@ -1,11 +1,15 @@
 package com.classification.handwriting
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HandwritingClassificationApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        // 항상 다크모드 해제
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
