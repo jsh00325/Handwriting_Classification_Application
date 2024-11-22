bf5619d5c7ab14bcc895576a16a12fdb40f69fea
Jeong Suhyeon <jsh00325@naver.com>
2024-11-23 00:55:04 +0900

    Chore: 초기 프로젝트 세팅

---
+++ app/src/main/java/com/classification/handwriting/HandwritingClassificationApplication.kt
@@ -0,0 +1,11 @@
+package com.classification.handwriting
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HandwritingClassificationApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/classification/handwriting/presentation/select_model/SelectModelActivity.kt
@@ -0,0 +1,13 @@
+package com.classification.handwriting.presentation.select_model
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.classification.handwriting.R
+
+class SelectModelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_model)
+
+    }
+}
